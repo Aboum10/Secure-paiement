3824136e9bc1a75d4224b4b5d8e0e023237ced4a
Aboum10 <aboum1000@gmail.com>
2026-05-09 16:01:08 +0000

    fix payment route

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sqlite3
 import random
 import smtplib
 from email.mime.text import MIMEText
-import os 
+import os
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
@@ -11,16 +11,37 @@ app.secret_key = "supersecretkey"
 # -------------------------------------------
 # Produits du shop
 products = [
-    {"name": "Smart Watch", "price": 120, "image": "https://images.unsplash.com/photo-1516574187841-cb9cc2ca948b"},
-    {"name": "Laptop", "price": 700, "image": "https://images.unsplash.com/photo-1517336714731-489689fd1ca8"},
-    {"name": "Desktop Computer", "price": 900, "image": "https://images.unsplash.com/photo-1587202372775-e229f172b9d7"},
-    {"name": "Smartphone", "price": 400, "image": "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"},
+    {
+        "name": "Smart Watch",
+        "price": 120,
+        "image": "https://images.unsplash.com/photo-1516574187841-cb9cc2ca948b"
+    },
+    {
+        "name": "Laptop",
+        "price": 700,
+        "image": "https://images.unsplash.com/photo-1517336714731-489689fd1ca8"
+    },
+    {
+        "name": "Desktop Computer",
+        "price": 900,
+        "image": "https://images.unsplash.com/photo-1587202372775-e229f172b9d7"
+    },
+    {
+        "name": "Smartphone",
+        "price": 400,
+        "image": "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
+    },
 ]
 
 # -------------------------------------------
 # Base de données
 def init_db():
-    conn = sqlite3.connect("database.db")
+
+    conn = sqlite3.connect(
+        "database.db",
+        check_same_thread=False
+    )
+
     cursor = conn.cursor()
 
     cursor.execute("""
@@ -41,8 +62,7 @@ def init_db():
 init_db()
 
 # -------------------------------------------
-
-# OTP
+# OTP + EMAIL
 def generate_otp(email):
 
     otp = str(random.randint(100000, 999999))
@@ -52,17 +72,18 @@ def generate_otp(email):
     print("=================================")
 
     sender = os.getenv("EMAIL_USER")
-
-    # MOT DE PASSE D’APPLICATION GOOGLE
     password = os.getenv("EMAIL_PASS")
 
     msg = MIMEText(f"Votre code OTP est : {otp}")
+
     msg["Subject"] = "Code OTP SecureShop"
     msg["From"] = sender
     msg["To"] = email
 
     try:
+
         server = smtplib.SMTP("smtp.gmail.com", 587)
+
         server.starttls()
 
         server.login(sender, password)
@@ -74,23 +95,24 @@ def generate_otp(email):
         print("EMAIL OTP ENVOYÉ")
 
     except Exception as e:
+
         print("ERREUR EMAIL :", e)
 
     return otp
 
 # -------------------------------------------
-# Routes
+# ROUTES
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
+# -------------------------------------------
 @app.route("/shop")
 def shop():
     return render_template("shop.html", products=products)
 
-
-@app.route("/payment", methods=["GET","POST"])
+# -------------------------------------------
+@app.route("/payment", methods=["GET", "POST"])
 def payment():
 
     product_prices = {
@@ -100,9 +122,14 @@ def payment():
         "Smartphone": 400
     }
 
-    # 🔥 CAS 1 : ARRIVÉE DEPUIS LE BOUTON (GET)
+    # -----------------------------------
+    # GET = choix produit
     if request.method == "GET":
+
         product = request.args.get("product")
+
+        if not product:
+            return redirect("/shop")
 
         if product not in product_prices:
             return "Produit invalide"
@@ -110,11 +137,14 @@ def payment():
         session["product"] = product
         session["amount"] = product_prices[product]
 
-        return render_template("payment.html",
-                               product=product,
-                               amount=product_prices[product])
+        return render_template(
+            "payment.html",
+            product=product,
+            amount=product_prices[product]
+        )
 
-    # 🔥 CAS 2 : FORMULAIRE ENVOYÉ (POST)
+    # -----------------------------------
+    # POST = formulaire paiement
     if request.method == "POST":
 
         session["name"] = request.form["name"]
@@ -124,24 +154,33 @@ def payment():
         session["cvv"] = request.form["cvv"]
 
         otp = generate_otp(session["email"])
+
         session["otp"] = otp
 
         return redirect("/otp")
 
-
+# -------------------------------------------
 @app.route("/otp", methods=["GET", "POST"])
 def otp():
 
     if request.method == "POST":
+
         user_otp = request.form["otp"]
 
-        conn = sqlite3.connect("database.db")
+        conn = sqlite3.connect(
+            "database.db",
+            check_same_thread=False
+        )
+
         cursor = conn.cursor()
 
+        # -----------------------------------
+        # OTP CORRECT
         if user_otp == session.get("otp"):
 
             cursor.execute("""
-                INSERT INTO transactions (name, card, exp, cvv, amount, status)
+                INSERT INTO transactions
+                (name, card, exp, cvv, amount, status)
                 VALUES (?, ?, ?, ?, ?, ?)
             """, (
                 session.get("name"),
@@ -156,11 +195,16 @@ def otp():
             conn.close()
 
             print("TRANSACTION SUCCESS SAVED")
+
             return redirect("/success")
 
+        # -----------------------------------
+        # OTP INCORRECT
         else:
+
             cursor.execute("""
-                INSERT INTO transactions (name, card, exp, cvv, amount, status)
+                INSERT INTO transactions
+                (name, card, exp, cvv, amount, status)
                 VALUES (?, ?, ?, ?, ?, ?)
             """, (
                 session.get("name"),
@@ -175,28 +219,33 @@ def otp():
             conn.close()
 
             print("TRANSACTION FAILED SAVED")
+
             return "OTP incorrect"
 
     return render_template("otp.html")
 
-
+# -------------------------------------------
 @app.route("/success")
 def success():
     return render_template("success.html")
 
-
+# -------------------------------------------
 @app.route("/admin")
 def admin():
     return redirect("/dashboard")
 
-
+# -------------------------------------------
 @app.route("/dashboard")
 def dashboard():
 
     if not session.get("admin"):
         return redirect("/login")
 
-    conn = sqlite3.connect("database.db")
+    conn = sqlite3.connect(
+        "database.db",
+        check_same_thread=False
+    )
+
     cursor = conn.cursor()
 
     cursor.execute("SELECT * FROM transactions")
@@ -224,27 +273,36 @@ def dashboard():
 def login():
 
     if request.method == "POST":
+
         username = request.form["username"]
         password = request.form["password"]
 
         if username == "admin" and password == "admin123":
+
             session["admin"] = True
+
             return redirect("/dashboard")
+
         else:
+
             return "Accès refusé"
 
     return render_template("login.html")
 
-
+# -------------------------------------------
 @app.route("/logout")
 def logout():
+
     session.clear()
+
     return redirect("/login")
 
 # -------------------------------------------
+# LANCEMENT APPLICATION
 if __name__ == "__main__":
-   app.run(
-    host="0.0.0.0",
-    port=5000,
-    debug=True
-)
+
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
