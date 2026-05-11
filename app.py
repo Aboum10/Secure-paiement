ff7fe98e16a3160ac0b6dfd319007805e84a6d44
Aboum10 <aboum1000@gmail.com>
2026-05-11 17:49:41 +0000

    Premier commit

--- app.py
+++ app.py
@@ -300,9 +300,4 @@ def logout():
 # -------------------------------------------
 # LANCEMENT APPLICATION
 if __name__ == "__main__":
-
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
+    app.run(host="0.0.0.0", port=5000)
